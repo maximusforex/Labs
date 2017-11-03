4d55bba18b5f6f6037b8bebd56dd9140078f0b2a
Maxim Malikov <malikovmd@mail.ru>
2017-11-03 19:00:17 +0200

    New Version

--- LabWork1.5.1/src/Sequence.java
+++ LabWork1.5.1/src/Sequence.java
@@ -1,10 +1,10 @@
 public class Sequence {
     public static void main(String[] args) {
-        for(int i=1;i<9;i++){
-            for(int j=1;j<i;j++){
-                System.out.print(j);
+        for(int i=0;i<8;i++){
+            for(int j=i+1;j>0;j--){
+                System.out.print(j+" ");
             }
-            System.out.println(i);
+            System.out.println();
         }
 
        /* for(int i=8;i>0;i--){
